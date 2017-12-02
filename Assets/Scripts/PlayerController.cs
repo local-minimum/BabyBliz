f413e6bfbfe012fdabfe47cfef3a3a1c9446dabe
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-02 13:53:23 +0100

    More player stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,15 +11,24 @@ public class PlayerController : MonoBehaviour {
     public PlayerBag bag;
     Rigidbody2D rb;
     Animation anim;
-    
+
+    float lastGrounded;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
-	
+
+    public Vector2 maxVelocities;
+
+    [Range(0, 1)]
+    public float jumpingWalkForceFactor = 0.5f;
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetButtonDown("Jump"))
+
+        bool grounded = Grounded;
+
+	    if (Input.GetButtonDown("Jump") && Grounded)
         {
             rb.AddForce(Vector2.up * jumpForce);
             
@@ -28,12 +37,42 @@ public class PlayerController : MonoBehaviour {
 
         if (Mathf.Abs(horizontal) > 0.01)
         {
-            rb.AddForce(Vector2.right * walkForce * horizontal);
+            rb.AddForce(Vector2.right * walkForce * horizontal * (grounded ? 1f : jumpingWalkForceFactor));
 
         }
 
-    
+        
+        if (grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, 0));
+        }
+
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxVelocities.x, maxVelocities.x), Mathf.Clamp(rb.velocity.y, -maxVelocities.y, maxVelocities.y));
     }
 
+    public float groundingTime = 0.1f;
 
+    public bool Grounded
+    {
+        get
+        {
+            return (Time.timeSinceLevelLoad - lastGrounded < groundingTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            lastGrounded = Time.timeSinceLevelLoad;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            lastGrounded = Time.timeSinceLevelLoad;
+        }
+    }
 }
