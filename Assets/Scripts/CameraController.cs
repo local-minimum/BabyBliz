b773ba327b392f54291b81f006139985b7fe01c8
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-02 17:32:20 +0100

    Bad cameras

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,46 @@ public class CameraController : MonoBehaviour {
 
     public Transform player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = player.transform.position + new Vector3(0, 0, -10);
+    [SerializeField]
+    Rect noMove;
+
+    [SerializeField]
+    float attack = 0.9f;
+
+    private void Start()
+    {
+        StartCoroutine(_updateTarget());
+    }
+
+    Vector3 playerTrackPosition;
+    Vector2 playerMovement;
+    Vector3 playerPosition;
+
+    [SerializeField]
+    float aimAttack = 0.4f;
+
+    IEnumerator<WaitForSeconds> _updateTarget()
+    {
+        while (true)
+        {
+            playerMovement = player.position - playerPosition;
+            playerTrackPosition = Vector3.Lerp(
+                playerTrackPosition, player.position, aimAttack);
+            playerPosition = player.position;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    Vector2 PlayerOnScren()
+    {
+        Vector2 playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
+        Vector2 relPos = new Vector2(playerPos.x / Screen.width, playerPos.y / Screen.height);
+        Vector2 direction = relPos - new Vector2(0.5f, 0.5f);
+        return relPos;
+    }
+
+    void Update () {
+
+        transform.position = player.position + new Vector3(0, 1f, -10f);
 	}
 }
