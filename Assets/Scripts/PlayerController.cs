d679723c22f87accdf5d0536f8e524bbd8e6c0d4
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-02 12:02:49 +0100

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Range(0, 3000)]
+    public float jumpForce = 2000;
+
+    Rigidbody2D rb;
+    Animation anim;
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            
+        }	
+	}
+
+
+}
