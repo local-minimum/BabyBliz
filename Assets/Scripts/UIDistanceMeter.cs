279fbc1a09ff2a61041bf2cdf2720f451fbd7016
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-02 12:44:58 +0100

    Added ui

---
+++ Assets/Scripts/UIDistanceMeter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDistanceMeter : MonoBehaviour {
+
+    Text myText;
+	// Use this for initialization
+	void Start () {
+        myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myText.text = string.Format("Walked: {0}", GameMaster.instance.distanceWalked);
+	}
+}
