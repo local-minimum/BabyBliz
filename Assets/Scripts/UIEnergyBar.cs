0fac19e8cc422294e5dd289994076643282e662a
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-04 17:09:07 +0100

    Better energy ui

--- Assets/Scripts/UIEnergyBar.cs
+++ Assets/Scripts/UIEnergyBar.cs
@@ -12,15 +12,36 @@ public class UIEnergyBar : MonoBehaviour {
     Image right;
 
     [SerializeField]
-    GameObject uiRoot;
+    Text message;
 
+    private void Start()
+    {
+        float progress = GameMaster.instance.willToLive;
+
+        SetProgress(progress);
+    }
 
     void Update()
     {
         float progress = GameMaster.instance.willToLive;
 
+        if (progress < 0.1f)
+        {
+            message.text = "No will to live";
+        } else if (progress < 0.3f)
+        {
+            message.text = "Need for coffee";
+        } else
+        {
+            message.text = "Will to live";
+        }
+        SetProgress(Mathf.Lerp(left.fillAmount, progress, 0.1f));
+  
+    }
+
+    void SetProgress(float progress)
+    {
         left.fillAmount = progress;
         right.fillAmount = progress;
-  
     }
 }
